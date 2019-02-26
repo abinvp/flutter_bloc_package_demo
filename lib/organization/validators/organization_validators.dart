682fc613d81b9050b732a2e026dcde18f7284678
Abin Paul <abinvp@gmail.com>
2019-02-26 22:53:21 +0530

    Removed unwanted dir

--- lib/organization/validators/organization_validators.dart
+++
@@ -1,8 +0,0 @@
-import 'dart:async';
-
-class OrganizationValidators {
-  static final validateOrganizationName =
-      StreamTransformer<String, String>.fromHandlers(handleData: (organizationName, sink) {
-    organizationName.isNotEmpty ? sink.add(organizationName) : sink.addError('Please enter a valid name.');
-  });
-}
